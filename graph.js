4392785bbcd8d0467f1a7fd3b9f5f27b0a60168e
Casey Chamberlain <caseygchamberlain@gmail.com>
2023-08-07 14:02:48 -0600

    stratify data

--- graph.js
+++ graph.js
@@ -3,10 +3,34 @@ const dims = { height: 500, width: 1100 };
 const svg = d3.select('canvas')
     .append('svg')
     .attr('width', dims.width + 100)
-    .attr('height', dims.height +100)
+    .attr('height', dims.height + 100);
 
-    const graph = svg.append('g')
-        .attr('transform', 'translate(50,50)')
+const graph = svg.append('g')
+    .attr('transform', 'translate(50,50)');
+
+
+//data strat
+const stratify = d3.stratify()
+    .id(d => d.name)
+    .parentId(d => d.parent);
+
+const tree = d3.tree()
+    .size([dims.width, dims.height])
+
+//update function
+const update = (data) => {
+
+    //get updated root Node data
+    const rootNode = stratify(data);
+    // console.log(rootNode)
+
+    const treeData= tree(rootNode);
+    // console.log(treeData)
+
+    //get nodes selection and join data
+    const nodes = graph.selectAll('.node')
+        .data(treeData.descendants());
+}
 
 
 var data = [];
@@ -31,4 +55,5 @@ db.collection('employees').onSnapshot(res => {
                 break;
         }
     })
+    update(data)
 })
